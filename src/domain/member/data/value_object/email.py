57e8615ac74c2a5d526aa515596a84525b4aac52
j4k0 <33048366+jak010@users.noreply.github.com>
2023-03-01 22:47:03 +0900

    Update email.py

--- src/domain/member/data/value_object/email.py
+++ src/domain/member/data/value_object/email.py
@@ -1,30 +1,23 @@
-from __future__ import annotations
-
 from dataclasses import dataclass
 
 
 @dataclass
 class EmailVo:
     email: str
-
-    local_part = None
-    domain_part = None
-    host = None
-    domain = None
+    local_part: str = ""
+    domain_part: str = ""
+    host: str = ""
+    domain: str = ""
 
     def __post_init__(self):
-        self._split_part(email=self.email)
+        self._split_part()
         self._split_domain()
 
         if self.host not in ["naver", "daum", "gmail"]:
-            raise Exception("Not Allowed Email Host")
+            raise ValueError("Not Allowed Email Host")
 
-    def _split_part(self, email):
-        local_part, domain_part = email.split("@")
-        self.local_part = local_part
-        self.domain_part = domain_part
+    def _split_part(self):
+        self.local_part, self.domain_part = self.email.split("@")
 
     def _split_domain(self):
-        host, domain = self.domain_part.split(".")
-        self.host = host
-        self.domain = domain
+        self.host, self.domain = self.domain_part.split(".")
